3c6d683ebbc5e79241a8f445760c1f5f08b6a536
ali1972 <m.abakhr@gmail.com>
2023-06-07 10:32:00 +0300

    اضافه صفحه تسجيل ثانيه

--- app/src/main/java/com/example/proreusev1/MainActivity.java
+++ app/src/main/java/com/example/proreusev1/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.proreusev1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int REGISTER_CODE = 675;
+    private Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // sdf
+
+        registerBtn = findViewById(R.id.register_btn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivityForResult(intent,REGISTER_CODE);
+
+            }
+        });
+
     }
 }
